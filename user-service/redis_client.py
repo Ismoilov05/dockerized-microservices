d530a76c0de988d31698b374e541d97c446873f2
@ <oe@qodir.local>
2026-06-12 17:43:56 +0500

    modified: redis_client.py

--- user-service/redis_client.py
+++ user-service/redis_client.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-
 from database import engine
 from models import Base
-
 from redis_client import redis_client
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
